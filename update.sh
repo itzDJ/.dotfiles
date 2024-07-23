8f7209a97ce823474b6fc47652282b16e1843996
dj <74993463+itzDJ@users.noreply.github.com>
2024-07-23 14:57:56 -0400

    fixed directory issue in install script

--- update.sh
+++ update.sh
@@ -7,24 +7,26 @@ git clone https://github.com/itzDJ/.dotfiles
 cd .dotfiles
 
 # General configs
-cp ~/.zshrc ~/.dotfiles/.zshrc
-cp ~/.config/alacritty/alacritty.toml ~/.dotfiles/alacritty.toml
+cp ~/.zshrc ~/.dotfiles/mac/.zshrc
+cp ~/.config/alacritty/alacritty.toml ~/.dotfiles/mac/alacritty.toml
 
 # OS specific configs
 if [[ "$OSTYPE" == "darwin"* ]]; then
     printf "MacOS detected\n"
     # Create a Brewfile (all installed packages)
     brew bundle dump
+    mv Brewfile ~/.dotfiles/mac
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     printf "Linux detected; Arch assumed\n"
     # Removing unused packages (orphans)
     yay -Qdtq | ifne yay -Rns -
     # Creates a file with all explicitly installed packages
     yay -Qqe > arch_packages.txt
+    mv arch_packages.txt ~/.dotfiles/arch
 
-    cp -r ~/.config/hypr ~/.dotfiles
-    cp -r ~/.config/waybar ~/.dotfiles
-    cp -r ~/.config/wofi ~/.dotfiles
+    cp -r ~/.config/hypr ~/.dotfiles/arch
+    cp -r ~/.config/waybar ~/.dotfiles/arch
+    cp -r ~/.config/wofi ~/.dotfiles/arch
 else
     printf "OS not supported. Exiting...\n"
     exit 1
