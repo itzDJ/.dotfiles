1fd78d8af2d7128ef45456e799a3f7a801d0b5e0
itzDJ <74993463+itzDJ@users.noreply.github.com>
2025-12-29 20:31:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,56 +1,69 @@
 #!/usr/bin/env bash
 
-# Exit on error
+# Exit on any error
 set -e
 
-# Exit if system isn't running archlinux
+# Ensure we are on Arch Linux
 if [[ ! -f /etc/arch-release ]]; then
-    echo "Error: Failed to detect /etc/arch-release"
+    echo "Error: This script is only for Arch Linux."
     exit 1
 fi
 
-# Cache sudo credentials
-sudo -v
-
-# Update system
+echo "Updating system..."
 sudo pacman -Syu --noconfirm
 
-# Install AUR helper
-sudo pacman -S --noconfirm --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd ..
-rm -rf yay
+# Install essential packages
+echo "Installing essential packages: git, base-devel, zsh..."
+sudo pacman -S --noconfirm --needed git base-devel zsh
 
-# Cleanup old bash files
-rm .bash*
-
-# Zsh setup
-yay -S --noconfirm --needed zsh
-sudo chsh -s $(which zsh) $USER
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Install packages (ignore lines that begin with a hashtag)
-git clone https://github.com/itzDJ/.dotfiles $HOME/.dotfiles
-yay -S --noconfirm --needed $(grep -v "^#" $HOME/.dotfiles/packages.txt)
-
-# Dotfiles symlinks
-ln -sf "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
-ln -sf "$HOME/.dotfiles/.zprofile" "$HOME/.zprofile"
-ln -sf "$HOME/.dotfiles/.config/hypr" "$HOME/.config/hypr"
-ln -sf "$HOME/.dotfiles/.config/waybar" "$HOME/.config/waybar"
-ln -sf "$HOME/.dotfiles/.config/wofi" "$HOME/.config/wofi"
-
-# Install neovim config (git submodules are annoying, so this is a workaround)
-git clone https://github.com/itzDJ/nvim ~/.config/nvim
-
-# Reboot to finish install
-echo -n "Rebooting in "
-for i in {3..1}; do
-    echo -n "$i..."
-    sleep 1
+# Create standard home directories
+echo "Creating home directories..."
+for dir in Downloads Scripts Software; do
+    mkdir -p "$HOME/$dir"
 done
-sudo systemctl reboot
+
+# Change default shell to zsh if not already
+if [[ "$SHELL" != "$(which zsh)" ]]; then
+    echo "Changing default shell to zsh..."
+    sudo chsh -s "$(which zsh)" "$USER"
+fi
+
+# Clone dotfiles early to use pkglist.txt
+DOTFILES="$HOME/.dotfiles"
+if [[ ! -d "$DOTFILES" ]]; then
+    echo "Cloning dotfiles..."
+    git clone https://github.com/itzDJ/.dotfiles "$DOTFILES"
+fi
+
+# Install packages from pkglist.txt
+if [[ -f "$DOTFILES/pkglist.txt" ]]; then
+    echo "Installing packages from pkglist.txt..."
+    sudo pacman -S --noconfirm --needed - < "$DOTFILES/pkglist.txt"
+fi
+
+# Install Brave browser from AUR
+BRAVE_DIR="$HOME/Software/brave-bin"
+if [[ ! -d "$BRAVE_DIR" ]]; then
+    echo "Cloning Brave AUR package..."
+    git clone https://aur.archlinux.org/brave-bin.git "$BRAVE_DIR"
+fi
+echo "Building and installing Brave..."
+makepkg -si --noconfirm -D "$BRAVE_DIR"
+
+# Symlink dotfiles
+echo "Creating symlinks for configuration..."
+ln -sf "$DOTFILES/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES/.zprofile" "$HOME/.zprofile"
+ln -sf "$DOTFILES/.config/hypr" "$HOME/.config/hypr"
+ln -sf "$DOTFILES/.config/waybar" "$HOME/.config/waybar"
+
+# Install Neovim configuration
+NVIM_CONFIG="$HOME/.config/nvim"
+if [[ ! -d "$NVIM_CONFIG" ]]; then
+    echo "Cloning Neovim config..."
+    git clone https://github.com/itzDJ/nvim "$NVIM_CONFIG"
+fi
+
+echo ""
+echo "Setup complete!"
+echo "Please log out and log back in to apply changes."
